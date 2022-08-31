63a152d02a712c56ba99a063f2b2c5e2a5886add
Raheem Alibhai <ral@ivoireacademie.com>
2022-08-31 13:49:57 +0800

    testing the page

--- app/controllers/loan_requests_controller.rb
+++ app/controllers/loan_requests_controller.rb
@@ -1,2 +1,25 @@
 class LoanRequestsController < ApplicationController
+  before_action :set_loan
+  def new
+    # To create a new loan request
+    # I need to find the loan id which comes from the url /loans/:loan_id/loan_requests/new(.:format
+    # Need to take into the pages
+    @loan_request = LoanRequest.new
+    @loan_request.loan_id = @loan
+    @loan_request.save
+    raise
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  private
+
+  def set_loan
+  @loan = Loan.find(params[:loan_id])
+  end
+
 end
