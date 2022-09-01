0343ace3e2a62f0a857576eaf9c2f8c2d87914ff
Bodil Hundevad <bodil@hundevad.de>
2022-09-01 10:13:50 +0800

    new: after login automatically redirect to borrower or lender dashboard depending on user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
+
   def after_sign_in_path_for(resource)
-    stored_location_for(resource) || dashboard_lender_path
+    if current_user.user_type == "lender"
+      path = dashboard_lender_path
+    else
+      path = dashboard_borrower_path
+    end
+
+    stored_location_for(resource) || path
   end
 
   def after_sign_out_path_for(resource)
