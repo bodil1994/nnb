f4e2d65755948b74dc63283533ca1130dce0267e
Eric Williamson <ericwilli@gmail.com>
2022-09-01 17:33:55 +0800

    added filter by loan type

--- app/javascript/controllers/search_loans_controller.js
+++ app/javascript/controllers/search_loans_controller.js
@@ -2,13 +2,33 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="search-loans"
 export default class extends Controller {
-  static targets = ["amountSlider", "amountShow", "form", "list"]
+  static targets = ["amountSlider", "amountShow", "lengthSlider", "lengthShow", "form", "list", "categories"]
 
   connect() {
   }
   updateAmount(event) {
     this.amountShowTarget.innerHTML = event.target.value
-    const url = `${this.formTarget.action}?amount=${event.target.value}`
+    this.updateList()
+  }
+  updateLength(event) {
+    this.lengthShowTarget.innerHTML = event.target.value
+    this.updateList()
+  }
+  updateCategory(event) {
+    console.log(this.categoriesTargets)
+    const categories = this.categoriesTargets
+    let checked_categories = ""
+    categories.forEach((category) => {
+      if(category.checked) {
+        checked_categories = checked_categories + `&loan%5Bloan_category%5D%5B%5D=${category.value}`
+      }
+    })
+    // console.log(checked_categories)
+    // this.updateList(checked_categories)
+  }
+  updateList(checked_categories){
+    const checked_categories = checked_categories || ""
+    const url = `${this.formTarget.action}?length=${this.lengthShowTarget.innerHTML}&amount=${this.amountShowTarget.innerHTML}${checked_categories}`
     fetch(url, {
       headers: { "Accept": "application/json" },
     })
