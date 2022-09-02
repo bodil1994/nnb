7fbb7a0d4ecf707a14f8a76b59cfec13442de40e
Raheem Alibhai <ral@ivoireacademie.com>
2022-09-02 16:15:21 +0800

    filtering done

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,6 +1,30 @@
 class WalletsController < ApplicationController
-  def new
-    @wallet = Wallet.new
-  end
 
+  def show
+    @user = current_user
+    @wallet = @user.wallet
+
+    # Create an empyt array called all_transactions
+    @all_transactions = []
+    #  deposts + with + transfers
+    # create an empty array called all_withdrawal and add it all the WithdrawalRequest
+    all_withdrawals = []
+    all_withdrawals = WithdrawalRequest.where(wallet_id: @user.wallet)
+
+    #   Add to all_transactions << all  deposit transaction related to the current user
+    all_deposits = []
+    all_deposits = Deposit.where(wallet_id: @user.wallet)
+    #   Add to all_transactions << all  transfers transaction related to the current user
+      # all_transfers = []
+      # all_transfers = Transfer.where(wallet_id: @user.wallet)
+    # Add all transactioin to the all_transactions array
+    @all_transactions << (all_withdrawals + all_deposits)
+
+    #use flatten in order to create a single array
+    @all_transactions = @all_transactions.flatten
+
+    #   Display thhe Filter the array by date desc with amount, date, status, "description = bank, mobil money, No Name Bank"
+    @all_transactions = @all_transactions.sort_by { |transaction| transaction.created_at }
+    @all_transactions = @all_transactions.last(5)
+  end
 end
