90b1b2fea7ec7cf1e410b2a57d6207b2988d2c9f
Eric Williamson <ericwilli@gmail.com>
2022-09-02 12:04:06 +0800

    added bank_accounts to seeds

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -2,7 +2,7 @@ class BankAccount < ApplicationRecord
   belongs_to :user
   has_many :withdrawal_requests
 
-  BANK_TYPES = ["Bank", "Mobile app"]
+  BANK_TYPES = ["Bank", "Mobile money"]
   validates :account_number, presence: true
   validates :bank_name, presence: true
   validates :bank_type, inclusion: { in: BANK_TYPES }
