75eb1ba989d9f61c6adf576eab36d5601a881ebd
Eric Williamson <ericwilli@gmail.com>
2022-09-02 11:10:24 +0800

    a few model changes

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,3 +1,9 @@
 class BankAccount < ApplicationRecord
   belongs_to :user
+  has_many :withdrawal_requests
+
+  BANK_TYPES = ["Bank", "Mobile app"]
+  validates :account_number, presence: true
+  validates :bank_name, presence: true
+  validates :bank_type, inclusion: { in: BANK_TYPES }
 end
