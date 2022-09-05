b0024aaeb669fc3309bfb92e3d13ed49389060d6
Franka Weiler <franka@tintentraum.de>
2022-09-05 09:27:36 +0200

    fix

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import CreateLoanController from "./create_loan_controller"
+application.register("create-loan", CreateLoanController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
