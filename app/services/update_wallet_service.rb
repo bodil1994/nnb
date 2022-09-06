4d78128ace964099f8e53a7e593fff2da92e1b5b
Raheem Alibhai <ral@ivoireacademie.com>
2022-09-06 11:12:25 +0800

    done

--- app/services/update_wallet_service.rb
+++ app/services/update_wallet_service.rb
@@ -8,16 +8,27 @@ class UpdateWalletService
   end
 
   def call()
-    if @transaction_type == "Transfer" && @borrower_transaction.transfer_type == "Deposit"
-      borrower_top_up = @borrower_wallet.amount + @borrower_transaction.amount
-      @borrower_wallet.update!(amount: borrower_top_up)
-      lender_top_down = @lender_wallet.amount - @lender_transaction.amount
-      @lender_wallet.update!(amount: lender_top_down)
-    else
-      lender_top_up = @lender_wallet.amount + @lender_transaction.amount
-      @lender_wallet.update!(amount: lender_top_up)
-      borrower_top_down = @borrower_wallet.amount - @borrower_transaction.amount
-      @borrower_wallet.update!(amount: borrower_top_down)
-    end
+    if @transaction_type == "Transfer"
+      if @borrower_transaction.transfer_type && @borrower_transaction.transfer_type == "Deposit"
+        loan_request
+      else
+        loan_payback
+      end
+    # elsif @transaction_type == ""
+    # end
+  end
+
+  def loan_request
+    borrower_top_up = @borrower_wallet.amount + @borrower_transaction.amount
+    @borrower_wallet.update!(amount: borrower_top_up)
+    lender_top_down = @lender_wallet.amount - @lender_transaction.amount
+    @lender_wallet.update!(amount: lender_top_down)
+  end
+
+  def loan_payback
+    lender_top_up = @lender_wallet.amount + @lender_transaction.amount
+    @lender_wallet.update!(amount: lender_top_up)
+    borrower_top_down = @borrower_wallet.amount - @borrower_transaction.amount
+    @borrower_wallet.update!(amount: borrower_top_down)
   end
 end
