a030666aa99fdf1f70379b9e78af546ac0738682
Eric Williamson <ericwilli@gmail.com>
2022-09-06 13:17:53 +0800

    dashboard and wallet buttons

--- app/javascript/controllers/avatar_dropdown_controller.js
+++ app/javascript/controllers/avatar_dropdown_controller.js
@@ -3,5 +3,10 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="avatar-dropdown"
 export default class extends Controller {
   connect() {
+    console.log("hello")
+  }
+
+  showMenu () {
+    console.log("hi")
   }
 }
