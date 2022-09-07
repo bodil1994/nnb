c649b8cf5cefe194cdfe972cb685a27331273ab9
Raheem Alibhai <ral@ivoireacademie.com>
2022-09-07 12:29:42 +0800

    intermediary push

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -8,7 +8,7 @@ class ChatroomsController < ApplicationController
 
   def index
     @user = current_user
-    @loans = Loan.where(user_id: @user.id)
+    @loans = Loan.where(user_id: @user.id, status: "Active")
   end
 
   def new
