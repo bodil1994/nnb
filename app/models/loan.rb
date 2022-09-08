b858390999b6223bc8a6ab84e9f86e524f270b49
Raheem Alibhai <ral@ivoireacademie.com>
2022-09-08 09:27:20 +0800

    message working with no styling

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -3,10 +3,7 @@ class Loan < ApplicationRecord
   has_many :loan_payments
   has_many :loan_requests
   has_one :chatroom
-<<<<<<< HEAD
-=======
 
->>>>>>> ae15127f6598acb8e34755bb80e9633d49afa785
   LOAN_CATEGORIES = ["Education", "Health", "Insurance", "Business"]
   LOAN_STATUS = ["Pending", "Listed", "Active", "Closed"]
   PAYMENY_FREQUENCY = ["Monthly", "Quarterly"]
