87d57286185ec37ef9c62da1475d52d24e888afe
Bodil Hundevad <bodil@hundevad.de>
2023-02-11 18:59:26 +0100

    Add  documentation for show action for user's loan request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  # GET / users / 1 / loan_requests / 1. Show information about a user's loan request on its own page when requested.
   def show
     @user = User.find(params[:id])
     @loan_request = LoanRequest.find(params[:loan_request_id])
