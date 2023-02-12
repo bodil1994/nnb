7848a61163b56c199fa0d7e26205b3f543ccaa69
Bodil Hundevad <bodil@hundevad.de>
2023-02-12 19:46:11 +0100

    Update database schema
    Refresh database to version 2022_09_06_065509
    Remove outdated schema versioning

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< Updated upstream
-ActiveRecord::Schema[7.0].define(version: 2022_09_06_132317) do
-=======
 ActiveRecord::Schema[7.0].define(version: 2022_09_06_065509) do
->>>>>>> Stashed changes
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
