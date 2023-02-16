a58fc857713ee4470044f66cc1922fc7dbcb363f
Bodil Hundevad <bodil@hundevad.de>
2023-02-16 22:39:02 +0100

    Remove validation for wallet amount
    No more need for wallet to have
    validation on amount field

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -2,5 +2,5 @@ class Wallet < ApplicationRecord
   belongs_to :user
   has_many :withrawal_requests
   has_many :deposits
-  validates :amount, numericality: { greater_than_or_equal_to: 0 }
+ # validates :amount, numericality: { greater_than_or_equal_to: 0 }
 end
