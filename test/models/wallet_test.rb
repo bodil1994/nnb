3a5bcba8fcd7324682ca1f029f99b458c473a462
Bodil Hundevad <bodil@hundevad.de>
2023-02-16 22:23:21 +0100

    Add tests for Wallet model
    - Add setup and teardown blocks for tests
    - Test for Wallet creation for a new user
    - Test for initial Wallet amount

--- test/models/wallet_test.rb
+++ test/models/wallet_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class WalletTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  # called before every single test
+  setup do
+    @user = User.create(first_name: "Sam", last_name: "Banana", profession: "Web developer", email: "info@sam.com", phone: "0199887682", address: "Sydney, Australia", password: "123123", user_type: "Lender")
+  end
+
+  # called after every single test
+  teardown do
+    @user = nil
+    # when controller is using cache it may be a good idea to reset it afterwards
+    Rails.cache.clear
+  end
+
+  test "Wallet is created when a new user is created" do
+    assert @user.wallet, "Wallet was not created"
+  end
+
+  test "Wallet initial amount is zero" do
+    assert_equal(0, @user.wallet.amount, "Wallet amount was not zero")
+  end
 end
